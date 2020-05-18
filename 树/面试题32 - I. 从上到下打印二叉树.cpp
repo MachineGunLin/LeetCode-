e815bad218f2afbe89718d4378044c289a5695e9
machine_gun_lin <805435831@qq.com>
2020-05-18 17:40:20 +0800

    Create 面试题32 - I. 从上到下打印二叉树.cpp

---
+++ 树/面试题32 - I. 从上到下打印二叉树.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> levelOrder(TreeNode* root) {
+        vector<int> res;
+        if(root == NULL) {
+            return res;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int length = q.size();
+            for(int i = 0; i < length; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                res.push_back(temp -> val);
+                if(temp -> left != NULL) q.push(temp -> left);
+                if(temp -> right != NULL) q.push(temp -> right);
+            }
+        }
+        return res;
+    }
+};
