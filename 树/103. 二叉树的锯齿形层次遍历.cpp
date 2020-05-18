79320a9b8ae179ac0e8c050a1cac0a44d576acef
machine_gun_lin <805435831@qq.com>
2020-05-18 16:03:50 +0800

    Create 103. 二叉树的锯齿形层次遍历.cpp

---
+++ 树/103. 二叉树的锯齿形层次遍历.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(root == NULL) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+        while(!q.empty()) {
+            int length = q.size();
+            deque<int> nodesOfThisLevel;
+            for(int i = 0; i < length; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                if(level % 2 == 0) {
+                    nodesOfThisLevel.push_back(temp -> val);
+                } else {
+                    nodesOfThisLevel.push_front(temp -> val);
+                }
+                if(temp -> left != NULL) q.push(temp -> left);
+                if(temp -> right != NULL) q.push(temp -> right);
+            }
+            res.push_back(vector<int>(nodesOfThisLevel.begin(), nodesOfThisLevel.end()));
+            level++;
+        }
+        return res;
+    }
+};
