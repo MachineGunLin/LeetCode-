9d18cf5339f58a54171c8ad7d2a598536336d720
machine_gun_lin <805435831@qq.com>
2020-05-18 17:31:30 +0800

    Create 1448. 统计二叉树中好节点的数目.cpp

---
+++ 树/1448. 统计二叉树中好节点的数目.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getGoodNodes(TreeNode* root, int currentMax) {
+        if(root == NULL) {
+            return 0;
+        }
+        int res = 0;
+        if(root -> val >= currentMax) {
+            res = 1;
+            currentMax = root -> val;
+        }
+        res += getGoodNodes(root -> left, currentMax) + getGoodNodes(root -> right, currentMax);
+        return res;
+    }
+    int goodNodes(TreeNode* root) {
+        return getGoodNodes(root, -10001);
+    }
+};
