c83783d0d3b13bdd74c6d0a4c46e692a986ec9d5
machine_gun_lin <805435831@qq.com>
2020-05-18 17:32:43 +0800

    Create 面试题 04.05. 合法二叉搜索树.cpp

---
+++ 树/面试题 04.05. 合法二叉搜索树.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValid(TreeNode* root, long lval, long rval) {
+        if(root == NULL) {
+            return true;
+        } else if(root -> val <= lval || root -> val >= rval) {
+            return false;
+        }
+        return isValid(root -> left, lval, root -> val) && isValid(root -> right, root -> val, rval);
+    }
+    bool isValidBST(TreeNode* root) {
+        return isValid(root, LONG_MIN, LONG_MAX);
+    }
+};
