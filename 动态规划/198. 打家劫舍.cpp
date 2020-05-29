c132e28d6879f985e84bfab68bed8a54075647c5
machine_gun_lin <805435831@qq.com>
2020-05-29 12:00:47 +0800

    Update 198. 打家劫舍.cpp

--- 动态规划/198. 打家劫舍.cpp
+++ 动态规划/198. 打家劫舍.cpp
@@ -29,3 +29,27 @@ public:
         return res;
     }
 };
+
+
+/*
+其实最后直接 return dp[size - 1]就行了，我傻了
+*/
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        int size = nums.size();
+        if (size == 1) {
+            return nums[0];
+        }
+        vector<int> dp = vector<int>(size, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < size; i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[size - 1];
+    }
+};
