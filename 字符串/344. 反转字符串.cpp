3f66a374618801c5530ecf079d437d35fd3d733d
machine_gun_lin <805435831@qq.com>
2020-06-05 12:18:10 +0800

    双指针

--- 字符串/344. 反转字符串.cpp
+++ 字符串/344. 反转字符串.cpp
@@ -8,3 +8,20 @@ public:
         reverse(s.begin(), s.end());
     }
 };
+
+
+/*
+双指针解法：
+*/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+        while(left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+};
