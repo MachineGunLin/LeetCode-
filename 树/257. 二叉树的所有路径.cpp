5b4034154e9329154196609d6defe7c183973ffd
machine_gun_lin <805435831@qq.com>
2020-08-21 15:19:19 +0800

    dfs

--- 树/257. 二叉树的所有路径.cpp
+++ 树/257. 二叉树的所有路径.cpp
@@ -1,3 +1,11 @@
+/*
+一顿dfs就行了，dfs参数里传一个string变量path记录从根节点到当前节点的路径的字符串。
+然后dfs里面加一些判断，如果是空就return，如果左右子树都为空就现在结果数组res里记录当前的路径path，然后return。
+然后在路径path上加下"->"，如果左子树不空就dfs左子树，如果右子树不空就dfs右子树。
+
+搜索完整个二叉树之后res数组中记录的就是答案。
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,21 +17,28 @@
  */
 class Solution {
 public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-        string path;
-        vector<string> res;
-        DFS(root, path, res);
-        return res;
-    }
-    void DFS(TreeNode* root, string path, vector<string>& res) {
-        if(root == NULL) return ;
-        path += to_string(root -> val);
-        path += "->";
-        if(root -> left == NULL && root -> right == NULL) {
-            path.erase(path.length() - 2);
-            res.push_back(path);
+    vector<string> res;
+
+    void dfs(TreeNode* root, string path) {
+        if(root == NULL) {
+            return ;
         }
-        DFS(root -> left, path, res);
-        DFS(root -> right, path, res);
+        path += to_string(root -> val);
+        if(root -> left == NULL && root -> right == NULL) {
+            res.push_back(path);
+            return;
+        }
+        path += "->";
+        if(root -> left != NULL) {
+            dfs(root -> left, path);
+        }
+        if(root -> right != NULL) {
+            dfs(root -> right, path);
+        }
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        dfs(root, "");
+        return res;
     }
 };
