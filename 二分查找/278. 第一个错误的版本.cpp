6167cc005120fca9580b4d5c6ba1a0effdcac6cd
machine_gun_lin <805435831@qq.com>
2020-08-28 09:46:19 +0800

    Create 278. 第一个错误的版本.cpp

---
+++ 二分查找/278. 第一个错误的版本.cpp
@@ -0,0 +1,22 @@
+/*
+调用题目给的API，二分查找第一个错误的版本。
+*/
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long left = 1, right = n;
+        while(left < right) {
+            int mid = (left + right) / 2;
+            if(isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+};
