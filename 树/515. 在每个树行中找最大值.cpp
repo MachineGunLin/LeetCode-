e8417503fe13f5bf0680176708010e771e4e27f9
machine_gun_lin <805435831@qq.com>
2020-11-11 22:40:11 +0800

    层次遍历

--- 树/515. 在每个树行中找最大值.cpp
+++ 树/515. 在每个树行中找最大值.cpp
@@ -1,43 +1,48 @@
+/*
+层次遍历，找出每一行的最大值。
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<vector<int>> nodes;
     vector<int> res;
+
     void BFS(TreeNode* root) {
-        if(root == NULL) return ;
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()) {
-            int length = q.size();
-            set<int> s;
-            for(int i = 0; i < length; i++) {
-                TreeNode* temp = q.front();
+            int size = q.size();
+            vector<int> nodesOfThiLevel;
+            for(int i = 0; i < size; ++i) {
+                auto t = q.front();
                 q.pop();
-                s.insert(temp -> val);
-                if(temp -> left != NULL) q.push(temp -> left);
-                if(temp -> right != NULL) q.push(temp -> right);
+                nodesOfThiLevel.push_back(t -> val);
+                if(t -> left != NULL) {
+                    q.push(t -> left);
+                }
+                if(t -> right != NULL) {
+                    q.push(t -> right);
+                }
             }
-            nodes.push_back(vector<int>(s.begin(), s.end()));
+            res.push_back(*max_element(nodesOfThiLevel.begin(), nodesOfThiLevel.end()));
         }
     }
-    void getRes(TreeNode* root) {
-        BFS(root);
-        int levels = nodes.size();
-        for(int i = 0; i < levels; i++) {
-            int length = nodes[i].size();
-            res.push_back(nodes[i][length - 1]);
-        }
-    }
+
     vector<int> largestValues(TreeNode* root) {
-        getRes(root);
+        if(root == NULL) {
+            return {};
+        }
+        BFS(root);
         return res;
     }
 };
